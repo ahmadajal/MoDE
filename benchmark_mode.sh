a53e0bbdd899b838d6004da193bb30f66b808899
ahmadajal <ajal.ahmad@gmail.com>
2020-07-24 09:05:24 +0000

    added new data to bash script

--- benchmark_mode.sh
+++ benchmark_mode.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 python benchmark_MoDE.py --data_path small_stock --normalize min-max
 python benchmark_MoDE.py --data_path big_stock --normalize min-max
+python benchmark_MoDE.py --data_path ../MoDE/Experiments_MATLAB/data/breast_cancer.mat
+python benchmark_MoDE.py --data_path ../MoDE/Experiments_MATLAB/data/cifar_train.mat
+python benchmark_MoDE.py --data_path ../MoDE/Experiments_MATLAB/data/eeg.mat
+python benchmark_MoDE.py --data_path ../MoDE/Experiments_MATLAB/data/hb.mat
+python benchmark_MoDE.py --data_path ../MoDE/Experiments_MATLAB/data/madelon_train.mat
+python benchmark_MoDE.py --data_path ../MoDE/Experiments_MATLAB/data/mnist.mat
+
+
