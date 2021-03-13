d2c70f25aeb39caaa4de7d49147f35143cbe5af3
ahmadajal <ajal.ahmad@gmail.com>
2021-03-13 10:06:49 +0000

    classification accuracy experiments

--- Experiments_Classification_Accuracy/prediction_experiment.py
+++ Experiments_Classification_Accuracy/prediction_experiment.py
@@ -19,8 +19,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from mode_inference import mode_inference
 from tsne_inference import tsne_inference
+import umap
 import argparse
-MATLAB_DATA_PATH = "../Experiments_MATLAB/data/"
+MATLAB_DATA_PATH = "../MATLAB_implementation/data/"
 DATA_PATH = "../data/"
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode_path', action="store")
@@ -44,72 +45,84 @@ test_data_score = loadmat(MATLAB_DATA_PATH+args.test_mat_path)["Score"].squeeze(
 train_data_labels = loadmat(MATLAB_DATA_PATH+args.train_mat_path)["labels"].squeeze()
 test_data_labels = loadmat(MATLAB_DATA_PATH+args.test_mat_path)["labels"].squeeze()
 
-#load ptsne embeddings
-train_data_ptsne = loadmat(DATA_PATH+args.ptsne_path[0])["mapped_train_X"]
-test_data_ptsne = loadmat(DATA_PATH+args.ptsne_path[1])["mapped_test_X"]
+# #load ptsne embeddings
+# train_data_ptsne = loadmat(DATA_PATH+args.ptsne_path[0])["mapped_train_X"]
+# test_data_ptsne = loadmat(DATA_PATH+args.ptsne_path[1])["mapped_test_X"]
 
 # compting the number of nearest neighbors given the perplexity value
 n_neighbors = min(len(train_data_labels) - 1, int(3. * args.perplexity + 1))
 # find number of classes
 nb_classes = len(set(train_data_labels))
 
-train_data_mode = loadmat(DATA_PATH+args.mode_path)["X_2d"]
-train_data_DM = loadmat(DATA_PATH+args.DM_path)["DM_average"]
-if scipy.sparse.issparse(train_data_mode):
-    train_data_mode = train_data_mode.toarray()
-    train_data_DM = train_data_DM.toarray()
-# t-SNE
-tsne = TSNE(n_components=2, perplexity=args.perplexity, n_iter=2000, random_state=None, verbose=1, metric="precomputed")
-train_data_tsne = tsne.fit_transform(train_data_DM)
-# ISOMAP
-isomap = Isomap(n_neighbors=n_neighbors, n_components=2, max_iter=2000, n_jobs=-1)
-train_data_isomap = isomap.fit_transform(train_data)
-test_data_isomap = isomap.transform(test_data)
+# train_data_mode = loadmat(DATA_PATH+args.mode_path)["X_2d"]
+# train_data_DM = loadmat(DATA_PATH+args.DM_path)["DM_average"]
+# if scipy.sparse.issparse(train_data_mode):
+#     train_data_mode = train_data_mode.toarray()
+#     train_data_DM = train_data_DM.toarray()
+# # t-SNE
+# tsne = TSNE(n_components=2, perplexity=args.perplexity, n_iter=2000, random_state=None, verbose=1, metric="precomputed")
+# train_data_tsne = tsne.fit_transform(train_data_DM)
+# # ISOMAP
+# isomap = Isomap(n_neighbors=n_neighbors, n_components=2, max_iter=2000, n_jobs=-1)
+# train_data_isomap = isomap.fit_transform(train_data)
+# test_data_isomap = isomap.transform(test_data)
+# UMAP
+reducer = umap.UMAP(n_neighbors=n_neighbors)
+train_data_umap = reducer.fit_transform(train_data)
+test_data_umap = reducer.transform(test_data)
+#temp
+plt.scatter(train_data_umap[:, 0], train_data_umap[:,1], c=train_data_labels,
+                   cmap=plt.cm.get_cmap("YlOrRd", nb_classes))
+plt.title("UMAP")
+plt.colorbar()
+plt.savefig("../results/" + args.mode_path.split("_")[0] + ".jpg", format="jpeg");
 # plot
-fig, ax = plt.subplots(2,2, figsize=(9, 9))
-if nb_classes > 10:
-    pallete = "YlOrRd"
-else:
-    pallete = "Set1"
-s1 = ax[0,0].scatter(train_data_tsne[:, 0], train_data_tsne[:,1], c=train_data_labels,
-                   cmap=plt.cm.get_cmap(pallete, nb_classes))
-ax[0,0].set_title("t-SNE")
-plt.colorbar(mappable=s1, ax=ax[0,0])
-##
-s2 = ax[0,1].scatter(train_data_mode[:,0], train_data_mode[:,1],
-                   c=train_data_labels, cmap=plt.cm.get_cmap(pallete, nb_classes))
-ax[0,1].set_title("MoDE")
-plt.colorbar(mappable=s2, ax=ax[0,1])
-##
-s3 = ax[1,0].scatter(train_data_ptsne[:,0], train_data_ptsne[:,1],
-                   c=train_data_labels, cmap=plt.cm.get_cmap(pallete, nb_classes))
-ax[1,0].set_title("pt-SNE")
-plt.colorbar(mappable=s3, ax=ax[1,0])
-##
-s4 = ax[1,1].scatter(train_data_isomap[:,0], train_data_isomap[:,1],
-                   c=train_data_labels, cmap=plt.cm.get_cmap(pallete, nb_classes))
-ax[1,1].set_title("ISOMAP")
-plt.colorbar(mappable=s4, ax=ax[1,1])
-plt.savefig("../figures/" + args.mode_path.split("_")[0] + ".jpg", format="jpeg");
+# fig, ax = plt.subplots(2,2, figsize=(9, 9))
+# if nb_classes > 10:
+#     pallete = "YlOrRd"
+# else:
+#     pallete = "Set1"
+# s1 = ax[0,0].scatter(train_data_tsne[:, 0], train_data_tsne[:,1], c=train_data_labels,
+#                    cmap=plt.cm.get_cmap(pallete, nb_classes))
+# ax[0,0].set_title("t-SNE")
+# plt.colorbar(mappable=s1, ax=ax[0,0])
+# ##
+# s2 = ax[0,1].scatter(train_data_mode[:,0], train_data_mode[:,1],
+#                    c=train_data_labels, cmap=plt.cm.get_cmap(pallete, nb_classes))
+# ax[0,1].set_title("MoDE")
+# plt.colorbar(mappable=s2, ax=ax[0,1])
+# ##
+# s3 = ax[1,0].scatter(train_data_ptsne[:,0], train_data_ptsne[:,1],
+#                    c=train_data_labels, cmap=plt.cm.get_cmap(pallete, nb_classes))
+# ax[1,0].set_title("pt-SNE")
+# plt.colorbar(mappable=s3, ax=ax[1,0])
+# ##
+# s4 = ax[1,1].scatter(train_data_isomap[:,0], train_data_isomap[:,1],
+#                    c=train_data_labels, cmap=plt.cm.get_cmap(pallete, nb_classes))
+# ax[1,1].set_title("ISOMAP")
+# plt.colorbar(mappable=s4, ax=ax[1,1])
+# plt.savefig("../figures/" + args.mode_path.split("_")[0] + ".jpg", format="jpeg");
 
-# generating test MoDE embeddings
-test_data_mode = mode_inference(test_data=test_data, train_data=train_data,
-                                       X_2d_mode=train_data_mode, k=n_neighbors)
-# generating test t-SNE embeddings
-test_data_tsne = tsne_inference(test_data=test_data, train_data=train_data,
-                                X_2d_tsne=train_data_tsne, k=n_neighbors)
+# # generating test MoDE embeddings
+# test_data_mode = mode_inference(test_data=test_data, train_data=train_data,
+#                                        X_2d_mode=train_data_mode, k=n_neighbors)
+# # generating test t-SNE embeddings
+# test_data_tsne = tsne_inference(test_data=test_data, train_data=train_data,
+#                                 X_2d_tsne=train_data_tsne, k=n_neighbors)
 # prediction
 results_test = {"full_data": {},
 "MoDE": {},
 "tSNE": {},
 "ptsne": {},
-"isomap": {}}
+"isomap": {},
+"umap": {}}
 
 results_train = {"full_data": {},
 "MoDE": {},
 "tSNE": {},
 "ptsne": {},
-"isomap": {}}
+"isomap": {},
+"umap": {}}
 
 LR = LogisticRegressionCV(Cs=np.logspace(-5, 5, 10), max_iter=4000, n_jobs=-1, verbose=0)
 KNN = KNeighborsClassifier(n_neighbors=n_neighbors)
@@ -160,6 +173,15 @@ KNN.fit(train_data_isomap, train_data_labels)
 results_test["isomap"]["KNN"] = KNN.score(test_data_isomap, test_data_labels)
 print("KNN accuracy for ISOMAP; ", results_test["isomap"]["KNN"])
 results_train["isomap"]["KNN"] = KNN.score(train_data_isomap, train_data_labels)
+
+LR.fit(train_data_umap, train_data_labels)
+results_test["umap"]["LR"] = LR.score(test_data_umap, test_data_labels)
+print("LR accuracy for UMAP; ", results_test["umap"]["LR"])
+results_train["umap"]["LR"] = LR.score(train_data_umap, train_data_labels)
+KNN.fit(train_data_umap, train_data_labels)
+results_test["umap"]["KNN"] = KNN.score(test_data_umap, test_data_labels)
+print("KNN accuracy for UMAP; ", results_test["umap"]["KNN"])
+results_train["umap"]["KNN"] = KNN.score(train_data_umap, train_data_labels)
 
 # save the results
 with open("../results/"+args.mode_path.split("_")[0]+ "_train" + ".json", "w") as f:
