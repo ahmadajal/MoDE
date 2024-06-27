0b8250576de6fccc14eb9d22a063b2ea5328ce0a
ahmadajal <ajal.ahmad@gmail.com>
2024-06-27 08:37:57 +0000

    pypi packaging files

---
+++ Python_implementation/setup.py
@@ -0,0 +1,51 @@
+"""Setup."""
+
+from setuptools import find_packages, setup, Extension
+from Cython.Build import cythonize
+import numpy
+import os
+
+# sourcefiles = [
+#     "src/MoDE_embeddings/fastgd/bootstrap.c",
+#     "src/MoDE_embeddings/fastgd/fastgd_base.c",
+#     "src/MoDE_embeddings/fastgd/fastgd_faster.c",
+#     "src/MoDE_embeddings/fastgd/fastgd_cython.c"
+# ]
+exts = [
+    Extension(
+            name="MoDE_embeddings.fastgd.bootstrap",
+            sources = ["src/MoDE_embeddings/fastgd/bootstrap.c"],
+            include_dirs=[numpy.get_include()]
+    ),
+    Extension(
+            name="MoDE_embeddings.fastgd.fastgd_base",
+            sources = ["src/MoDE_embeddings/fastgd/fastgd_base.c"],
+            include_dirs=[numpy.get_include()]
+    ),
+    Extension(
+            name="MoDE_embeddings.fastgd.fastgd_faster",
+            sources = ["src/MoDE_embeddings/fastgd/fastgd_faster.c"],
+            include_dirs=[numpy.get_include()]
+    ),
+    Extension(
+            name="MoDE_embeddings.fastgd.fastgd_cython",
+            sources = ["src/MoDE_embeddings/fastgd/fastgd_cython.c"],
+            include_dirs=[numpy.get_include()]
+    ),
+]
+
+c_exts = cythonize(exts)
+
+setup(
+    name="MoDE_embeddings",
+    version="0.1.4",
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    package_data={'MoDE_embeddings.fastgd': [
+        '*.pyx',
+        '*.pxd',
+        '*.c'
+    ]},
+    ext_modules=c_exts,
+    include_package_data=True,
+)
