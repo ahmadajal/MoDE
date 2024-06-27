22a44d6011b960c8a5ec22efc0eb3cd0a94178be
ahmadajal <ajal.ahmad@gmail.com>
2024-06-27 11:21:23 +0000

    change version

--- Python_implementation/setup.py
+++ Python_implementation/setup.py
@@ -1,9 +1,8 @@
 """Setup."""
 
-from setuptools import find_packages, setup, Extension
-from Cython.Build import cythonize
 import numpy
-import os
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
 
 # sourcefiles = [
 #     "src/MoDE_embeddings/fastgd/bootstrap.c",
@@ -13,24 +12,24 @@ import os
 # ]
 exts = [
     Extension(
-            name="MoDE_embeddings.fastgd.bootstrap",
-            sources = ["src/MoDE_embeddings/fastgd/bootstrap.c"],
-            include_dirs=[numpy.get_include()]
+        name="MoDE_embeddings.fastgd.bootstrap",
+        sources=["src/MoDE_embeddings/fastgd/bootstrap.c"],
+        include_dirs=[numpy.get_include()],
     ),
     Extension(
-            name="MoDE_embeddings.fastgd.fastgd_base",
-            sources = ["src/MoDE_embeddings/fastgd/fastgd_base.c"],
-            include_dirs=[numpy.get_include()]
+        name="MoDE_embeddings.fastgd.fastgd_base",
+        sources=["src/MoDE_embeddings/fastgd/fastgd_base.c"],
+        include_dirs=[numpy.get_include()],
     ),
     Extension(
-            name="MoDE_embeddings.fastgd.fastgd_faster",
-            sources = ["src/MoDE_embeddings/fastgd/fastgd_faster.c"],
-            include_dirs=[numpy.get_include()]
+        name="MoDE_embeddings.fastgd.fastgd_faster",
+        sources=["src/MoDE_embeddings/fastgd/fastgd_faster.c"],
+        include_dirs=[numpy.get_include()],
     ),
     Extension(
-            name="MoDE_embeddings.fastgd.fastgd_cython",
-            sources = ["src/MoDE_embeddings/fastgd/fastgd_cython.c"],
-            include_dirs=[numpy.get_include()]
+        name="MoDE_embeddings.fastgd.fastgd_cython",
+        sources=["src/MoDE_embeddings/fastgd/fastgd_cython.c"],
+        include_dirs=[numpy.get_include()],
     ),
 ]
 
@@ -38,14 +37,10 @@ c_exts = cythonize(exts)
 
 setup(
     name="MoDE_embeddings",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(where="src"),
-    package_dir={'': 'src'},
-    package_data={'MoDE_embeddings.fastgd': [
-        '*.pyx',
-        '*.pxd',
-        '*.c'
-    ]},
+    package_dir={"": "src"},
+    package_data={"MoDE_embeddings.fastgd": ["*.pyx", "*.pxd", "*.c"]},
     ext_modules=c_exts,
     include_package_data=True,
 )
